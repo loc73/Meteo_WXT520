7871742d1e3fe1a216fb1e696f96dee4511f721d
loc73 <lonardoni.loic@gmail.com>
2022-12-22 12:25:30 +0100

    première sauvegarde

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,17 @@
 #include <Arduino.h>
 
 void setup() {
-  // put your setup code here, to run once:
+  Serial.begin(9600);
+  Serial1.begin(9600);
+  pinMode(LED_BUILTIN, OUTPUT);
+  
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  digitalWrite(LED_BUILTIN, HIGH);
+  Serial.println("hello world 0");
+  Serial1.println("hello world 1");
+  delay(2000);
+  digitalWrite(LED_BUILTIN, LOW);
+  delay(2000);
 }
