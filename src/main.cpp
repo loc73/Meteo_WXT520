9659b342f99f4696184b3e7797f293f6e62c3bb7
loc73 <lonardoni.loic@gmail.com>
2023-01-14 22:15:31 +0100

    organisation des commandes et envoie des données

--- src/main.cpp
+++ src/main.cpp
@@ -8,8 +8,7 @@
 /********************  Définition des variables *******************************/
 /******************************************************************************/
 
-String chaine;
-int cptr =0;
+
 
 //déclaration d'un objet capteur méteo
 WXT520 My_WXT520;
@@ -17,17 +16,17 @@ WXT520 My_WXT520;
 WiFiClient espClient; // cration du client Wifi
 PubSubClient client(espClient);
 
-// led embarquée ESP32
-const int On_Board_LED    = LED_BUILTIN; // Arduino pin connected to LED's pin
-int ledState = LOW;     // the current state of LED
-
-char Var_MQTT[10]; //tableau pour la conversion des valeurs
 
 long lastReconnectAttempt = 0; // variable permettant d'avoir un delay avant une reconnexion.
 
-char Dmd_vent[] = "0R1";
-char Dmd_temp[] = "0R2";
-char Dmd_preci[] = "0R3";
+
+String chaine;
+
+char Var_MQTT[10]; //tableau pour la conversion des valeurs
+
+int cptr =0;
+int ledState = LOW;     // the current state of LED
+const int On_Board_LED    = LED_BUILTIN; // Arduino pin connected to LED's pin
 
 unsigned long currentTime=0;
 
@@ -44,6 +43,8 @@ bool envoie_temp = false;
 bool envoie_vent = false;
 bool envoie_preci = false;
 
+float Previous_vent = 0;
+float Previous_intens_preci = 0;
 
 unsigned int Cptr_temp = 99;
 unsigned int retard_envoi = 0;
@@ -75,29 +76,50 @@ void reconnect() {
   }
 }
 
-// fonction permettant de gérer les infos arrivant du topic auquel on a souscrit
-void callback(char* topic, byte* payload, unsigned int length) {
-  Serial.print("Message arrived [");
-  Serial.print(topic);
-  Serial.print("] ");
-  if (!strncmp((char *)payload, "infos", length)){
-    infos_completes = true;
-  }
-  else if (!strncmp((char *)payload, "temp", length)){ // on force l'envoie de la température
-    envoie_temp = true;
-  }
-  else if (!strncmp((char *)payload, "vent", length)){ // on force l'envoie du vent
-    envoie_vent = true;
-  }
-  else if (!strncmp((char *)payload, "preci", length)){ // on force l'envoi des précipitation.
-    envoie_preci = true;
-  }
-  // sinon on envoie la commande sur la liaison série cela peux être utile pour reconfigurer la station
-  else{
+// 1=temp, 2=vent, 3=preci, 4= RAZ mesures
+void dmd_manuelle(int choix){
+  int length = 0;
+  if (choix == 1){
+    char Dmd_temp[] = "0R2";
+    length = sizeof(Dmd_temp);  
     for (int i=0;i<length;i++) {
-      Serial2.print((char)payload[i]);
+      Serial2.print((char)Dmd_temp[i]);
     }
     Serial2.println();
+    delay(150);
+    envoie_temp = true;      
+  }
+
+  else if (choix == 2){
+    char Dmd_vent[] = "0R1";
+    length = sizeof(Dmd_vent);  
+    for (int i=0;i<length;i++) {
+      Serial2.print((char)Dmd_vent[i]);
+    }
+    Serial2.println();
+    delay(150);
+    envoie_vent = true;
+  }
+
+  else if (choix == 3){
+    char Dmd_preci[] = "0R3";
+    length = sizeof(Dmd_preci);  
+    for (int i=0;i<length;i++) {
+      Serial2.print((char)Dmd_preci[i]);
+    }
+    Serial2.println();
+    delay(150);
+    envoie_preci = true;
+  }
+
+  else if (choix == 4){
+    char Dmd_raz[] = "0XZRU";
+    length = sizeof(Dmd_raz);  
+    for (int i=0;i<length;i++) {
+      Serial2.print((char)Dmd_raz[i]);
+    }
+    Serial2.println();
+    delay(150);
   }
 }
 
@@ -121,40 +143,49 @@ void recup_tri_donnees(){
   }
 }
 
-// 1=temp, 2=vent, 3=preci
-void dmd_manuelle(int choix){
-    int length = 0;
 
-    if (choix == 1){
-      length = sizeof(Dmd_temp);  
-      for (int i=0;i<length;i++) {
-        Serial2.print((char)Dmd_temp[i]);
-      }
-      Serial2.println();
-      delay(150);
-      envoie_temp = true;      
+// fonction permettant de gérer les infos arrivant du topic auquel on a souscrit
+void callback(char* topic, byte* payload, unsigned int length) {
+  Serial.print("Message arrived [");
+  Serial.print(topic);
+  Serial.print("] ");
+  if (!strncmp((char *)payload, "infos", length)){
+    infos_completes = true;
+  }
+  else if (!strncmp((char *)payload, "temp", length)){ // on force l'envoie de la température
+    dmd_manuelle(1);
+    envoie_temp = true;
+  }
+  else if (!strncmp((char *)payload, "vent", length)){ // on force l'envoie du vent
+    dmd_manuelle(2);
+    envoie_vent = true;
+  }
+  else if (!strncmp((char *)payload, "preci", length)){ // on force l'envoi des précipitation.
+    dmd_manuelle(3);
+    envoie_preci = true;
+  }
+  else if (!strncmp((char *)payload, "raz", length)){ //on force une remise à zéro des données
+    dmd_manuelle(4);
+  }
+  // sinon on envoie la commande sur la liaison série cela peux être utile pour reconfigurer la station
+  else{
+    for (int i=0;i<length;i++) {
+      Serial2.print((char)payload[i]);
     }
-
-    else if (choix == 2){
-      length = sizeof(Dmd_vent);  
-      for (int i=0;i<length;i++) {
-        Serial2.print((char)Dmd_vent[i]);
-      }
-      Serial2.println();
-      delay(150);
-      envoie_vent = true;
-    }
-
-    else if (choix == 3){
-      length = sizeof(Dmd_preci);  
-      for (int i=0;i<length;i++) {
-        Serial2.print((char)Dmd_preci[i]);
-      }
-      Serial2.println();
-      delay(150);
-      envoie_preci = true;
-    }
+    Serial2.println();
+  }
 }
+
+//allume la led, attend une certaine durée en ms puis éteint la led
+void blink_and_Wait(int Led, int t_ms){
+  digitalWrite(Led, HIGH);
+  delay(t_ms/2);
+  digitalWrite(Led, LOW);
+  delay(t_ms/2);
+}
+
+
+
 
 
 /******************************************************************************/
@@ -196,9 +227,12 @@ void loop() {
   if((currentTime-previousTime_temp) > Interval_temp_15min){
     previousTime_temp=currentTime;
     Tps_15min_ok = true;
+    retard_envoi++;// on incrémente le compteur de délai
     cptr_1h++;
     if (cptr_1h > 3){
       envoie_vent = true;
+      envoie_preci = true;
+      cptr_1h = 0; //raz compteur durée une heure (4x15min)
     }
   }
 
@@ -218,35 +252,25 @@ void loop() {
 
   // si on fait une demande de toutes les infos alors on interroge les différents buffer.
   if(infos_completes){
-    dmd_manuelle(1);
-    delay(150);
-    recup_tri_donnees();
+    //dmd_manuelle(1);  // pas besoin de demander les infos de température car elles seront demandés par la suite.
+    //delay(150);
+    //recup_tri_donnees();
     dmd_manuelle(2);
     delay(150);
     recup_tri_donnees();
     dmd_manuelle(3);
     delay(150);
     recup_tri_donnees();
+    envoie_temp = true;
+    envoie_vent = true;
+    envoie_preci = true;
+
     infos_completes = false;
   }
   // sinon on regarde si il y a qqch sur la liaison série
   // on doit au minimum avoir des données toutes les minutes.
   else{
     recup_tri_donnees();
-    // si le vent est supérieur à 1 m/s ou 1km/h alors on envoie toutes les valeurs correspondants au vent
-    if (My_WXT520.Vitesse_Vent_moy() > 1){
-      dmd_manuelle(2);
-      delay(150);
-      envoie_vent = true;
-    }
-    // si il y a des précipitations alors on envoie plus souvent toutes les valeurs
-    if (My_WXT520.Intensite_precipitation() > 0){
-      dmd_manuelle(3);
-      delay(150);
-      recup_tri_donnees();
-      envoie_preci = true;
-    }
-
   }
 
 
@@ -256,12 +280,9 @@ void loop() {
     // envoie de la température
     dtostrf(My_WXT520.Temperature(), 1, 2, Var_MQTT);
     client.publish("WXT520/Temp",Var_MQTT);
-    ledState = !ledState;
-    digitalWrite(On_Board_LED, ledState);
-    delay(500);
-    retard_envoi++;// on incrémente le compteur de délai
+    blink_and_Wait(On_Board_LED, 500);
     // on envoie une fois sur 2 l'himidité et la pression
-    if (retard_envoi >1){ 
+    if (retard_envoi >1 || envoie_temp){ 
       retard_envoi = 0;
       dmd_manuelle(1);
       delay(150);
@@ -269,43 +290,58 @@ void loop() {
       // envoie de la humidité
       dtostrf(My_WXT520.Humidite(), 1, 2, Var_MQTT);
       client.publish("WXT520/Humidite",Var_MQTT);
-      ledState = !ledState;
-      digitalWrite(On_Board_LED, ledState); 
-      delay(500);
+      blink_and_Wait(On_Board_LED, 500);
       // envoie de la pression
       dtostrf(My_WXT520.Pression(), 1, 2, Var_MQTT);
       client.publish("WXT520/Pression",Var_MQTT);
-      ledState = !ledState;
-      digitalWrite(On_Board_LED, ledState); 
-      delay(500);
+      blink_and_Wait(On_Board_LED, 500);
     }
     Tps_15min_ok = false;
     envoie_temp = false;
   }
 
+
+      // si le vent est supérieur à 5 m/s ou 5km/h alors on envoie toutes les valeurs correspondants au vent
+    if (My_WXT520.Vitesse_Vent_moy() > 5 && My_WXT520.Vitesse_Vent_moy() != Previous_vent){
+      dmd_manuelle(2);
+      delay(150);
+      recup_tri_donnees();
+      envoie_vent = true;
+      Previous_vent = My_WXT520.Vitesse_Vent_moy();
+    }
+    // si il y a des précipitations alors on envoie plus souvent toutes les valeurs
+    if (My_WXT520.Intensite_precipitation() > 0 && My_WXT520.Intensite_precipitation() != Previous_intens_preci){
+      dmd_manuelle(3);
+      delay(150);
+      recup_tri_donnees();
+      envoie_preci = true;
+      Previous_intens_preci = My_WXT520.Intensite_precipitation();
+    }
+
+
   // publication des infos de vent si supérieur à 1km/h
   if (envoie_vent){
-    dtostrf(My_WXT520.Direction_Vent_moy(), 1, 2, Var_MQTT);
+    dtostrf(My_WXT520.Direction_Vent_moy(), 1, 2, Var_MQTT);  // envoie direction vent
+    client.publish("WXT520/Dir_vent",Var_MQTT);
+    blink_and_Wait(On_Board_LED, 500);
+    dtostrf(My_WXT520.Vitesse_Vent_moy(), 1, 2, Var_MQTT);  // envoie vitesse moyen vent
     client.publish("WXT520/Vent_moy",Var_MQTT);
-    ledState = !ledState;
-    digitalWrite(On_Board_LED, ledState); 
-    delay(500);
+    blink_and_Wait(On_Board_LED, 500);
     envoie_vent = false;
-    cptr_1h = 0; //raz durée une heure
   }
 
   // publication des infos de précipitation si > à 0mm/h
   if(envoie_preci){
+   
+    dtostrf(My_WXT520.Intensite_precipitation(), 1, 2, Var_MQTT);
+    client.publish("WXT520/intens_preci",Var_MQTT);
+    blink_and_Wait(On_Board_LED, 500);
     dtostrf(My_WXT520.Accumulation_precipitation(), 1, 2, Var_MQTT);
     client.publish("WXT520/Accu_preci",Var_MQTT);
-    ledState = !ledState;
-    digitalWrite(On_Board_LED, ledState); 
-    delay(500);
+    blink_and_Wait(On_Board_LED, 500);
     dtostrf(My_WXT520.Duree_precipitation(), 1, 2, Var_MQTT);
     client.publish("WXT520/Duree_preci",Var_MQTT);
-    ledState = !ledState;
-    digitalWrite(On_Board_LED, ledState); 
-    delay(500);
+    blink_and_Wait(On_Board_LED, 500);
     envoie_preci = false;
   } 
 }  
